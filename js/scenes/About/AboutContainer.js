ca58dd9d3a0902b82e87c600c835474e1ba96a29
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-26 16:52:21 -0800

    added boiler plate for AboutContainer

---
+++ js/scenes/About/AboutContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import About from "./About";
+
+class AboutContainer extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <About />;
+  }
+}
