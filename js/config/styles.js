6e2af23ce73f7943073e5c6ace895b35b6b133c2
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-27 14:49:22 -0800

    added fonts into config.js

--- js/config/styles.js
+++ js/config/styles.js
@@ -1,5 +1,13 @@
+import { lightMont } from "../assets/fonts/Montserrat-Light.ttf";
+import { regMont } from "../assets/fonts/Montserrat-Regular.ttf";
+
 export const colors = {
   black: "#000000",
   nearBlack: "#111111",
   mediumGrey: "#999999"
+};
+
+export const fonts = {
+  lightMont: lightMont,
+  regMont: regMont
 };
