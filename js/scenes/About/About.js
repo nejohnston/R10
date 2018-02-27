e044b9713b37085892c42cb7d61c2f87218063fb
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-26 22:35:24 -0800

    added all styles to text fields within about.js

--- js/scenes/About/About.js
+++ js/scenes/About/About.js
@@ -12,8 +12,22 @@ const About = ({ data }) => (
         source={require("../../assets/images/r10_logo.png")}
       />
     </View>
-    {data.map(data => <View />)}
-    <Text>I am on the about scene</Text>
+    <Text style={styles.bodyText}>
+      R10 is a conference that focuses on just about any topic related to dev.
+    </Text>
+    <Text style={styles.openingTitle}>Date & Venue</Text>
+    <Text style={styles.bodyText}>
+      The R10 conference will take place on the North Pole, Mrs. Claus will be
+      hosting.
+    </Text>
+    <Text style={styles.openingTitle}>Date & Venue</Text>
+
+    {data.map(data => (
+      <View>
+        <Text style={styles.descriptionTitle}>{data.title}</Text>
+        <Text style={styles.bodyText}>{data.description}</Text>
+      </View>
+    ))}
   </View>
 );
 
--- js/scenes/About/styles.js
+++ js/scenes/About/styles.js
@@ -4,10 +4,24 @@ const { height, width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   headerView: {
-    height: height / 4,
-    borderWidth: 2,
+    height: height / 5,
+    borderBottomWidth: 0.5,
+    marginHorizontal: 20,
     justifyContent: "center",
     alignItems: "center"
+  },
+  openingTitle: {
+    fontSize: 20,
+    marginBottom: 20
+  },
+  bodyText: {
+    fontSize: 14,
+    lineHeight: 3,
+    marginBottom: 20
+  },
+  descriptionTitle: {
+    color: "#C8A2C8",
+    marginBottom: 20
   }
 });
 
