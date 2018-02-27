4faed0d94bbd2106b1439ba7c31aa3f41be37642
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-26 16:56:41 -0800

    added boiler plate for About About/index and styles.js

---
+++ js/scenes/About/About.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+const About = () => (
+  <View>
+    <Text>I am on the about scene</Text>
+  </View>
+);
