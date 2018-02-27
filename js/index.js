b5fed913c4cbf87b764e26abbe8bbae111d6e979
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-26 16:47:39 -0800

    first commit for this awesome project

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
