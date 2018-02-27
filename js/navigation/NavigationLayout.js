14db9b4f3efd671cc5f700fe914bf98dc2c2380d
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-27 11:50:41 -0800

    added rough setup of navigationlayout.js

---
+++ js/navigation/NavigationLayout.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class NavigationLayout extends Component {
+    render() {
+        return(
+            // Put your tab bar / tab items / stack navs here~~~~~~
+        )
+    }
+}
+
+export default NavigationLayout;
