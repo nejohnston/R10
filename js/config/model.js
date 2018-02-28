d3374535e55bb055861fb34d035058d037f30e62
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-28 11:12:47 -0800

    added queryFave createFave deleteFave to model.js

--- js/config/model.js
+++ js/config/model.js
@@ -10,5 +10,24 @@ const FaveSchema = {
 };
 
 const realm = new Realm({ schema: [FaveSchema] });
+console.log(realm.path);
+
+export const queryFave = () => {
+  return realm.objects("Fave");
+};
+export const createFave = id => {
+  realm.write(() => {
+    realm.create("Fave", {
+      fave_id: id,
+      faved_on: new Date()
+    });
+  });
+};
+export const deleteFave = id => {
+  realm.write(() => {
+    realm.objects("Fave").filtered("fave_id == $0", id);
+    realm.delete();
+  });
+};
 
 export default realm;
