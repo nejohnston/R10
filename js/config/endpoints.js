c92452ead490370739ab9e8afefeaa6db4140fb5
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-27 17:21:38 -0800

    made variables for all firebase endpoints

---
+++ js/config/endpoints.js
@@ -0,0 +1,7 @@
+export const sessionsEndpoint =
+  "https://r10app-95fea.firebaseio.com/sessions.json";
+export const speakersEndpoint =
+  "https://r10app-95fea.firebaseio.com/speakers.json";
+export const singleSpeakersEndpoint = `https://r10app-95fea.firebaseio.com/speakers.json?orderBy="${speaker_id}"&equalTo="-KZ2o1CzG5GOfmURNSUB"`;
+export const aboutEndpoint =
+  "https://r10app-95fea.firebaseio.com/code_of_conduct.json";
