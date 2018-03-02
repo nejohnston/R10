d95453decd9b53a9a78adda905494858fc554b32
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-03-02 09:43:12 -0800

    added a firebase_url constant to endpoints.js

--- js/config/endpoints.js
+++ js/config/endpoints.js
@@ -1,7 +1,6 @@
-export const SCHEDULE_ENDPOINT =
-  "https://r10app-95fea.firebaseio.com/sessions.json";
-export const SPEAKERS_ENDPOINT =
-  "https://r10app-95fea.firebaseio.com/speakers.json";
-// export const SINGLE_SPEAKER_ENDPOINT = `https://r10app-95fea.firebaseio.com/speakers.json?orderBy="${speaker_id}"&equalTo="-KZ2o1CzG5GOfmURNSUB"`;
-export const ABOUT_ENDPOINT =
-  "https://r10app-95fea.firebaseio.com/code_of_conduct.json";
+const FIREBASE_URL = "https://r10app-95fea.firebaseio.com/";
+
+export const SCHEDULE_ENDPOINT = `${FIREBASE_URL}sessions.json`;
+export const SPEAKERS_ENDPOINT = `${FIREBASE_URL}speakers.json`;
+export const SINGLE_SPEAKER_ENDPOINT = `${FIREBASE_URL}speakers.json?orderBy="${speaker_id}"&equalTo="-KZ2o1CzG5GOfmURNSUB"`;
+export const ABOUT_ENDPOINT = `${FIREBASE_URL}code_of_conduct.json`;
