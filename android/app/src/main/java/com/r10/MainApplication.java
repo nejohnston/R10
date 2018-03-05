271241364b34bece93b6ffb59b229e2fad8da64e
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-03-04 18:09:19 -0800

    linked react-native-linear-gradient

--- android/app/src/main/java/com/r10/MainApplication.java
+++ android/app/src/main/java/com/r10/MainApplication.java
@@ -5,6 +5,9 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import io.realm.react.RealmReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import io.realm.react.RealmReactPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -31,6 +34,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
+            new VectorIconsPackage(),
+            new LinearGradientPackage(),
             new RealmReactPackage(),
             new VectorIconsPackage()
       );
