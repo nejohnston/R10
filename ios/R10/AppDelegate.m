62e28e091a466aafc4113aaeab4e7f1aeb23277d
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-03-08 10:31:37 -0800

    added '+ @import GoogleMaps;' to my ios/R10/AppDelegate.m file

--- ios/R10/AppDelegate.m
+++ ios/R10/AppDelegate.m
@@ -11,7 +11,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
++ @import GoogleMaps; //add this line if you want to use Google Maps
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
